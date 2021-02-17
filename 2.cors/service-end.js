e8aafed92f81b567387c12dbce9b5c7fb5513e41
Cherish0207 <cheshire_1207@foxmail.com>
2021-02-17 15:28:38 +0800

    cors.2.服务端res.setHeader(Access-Control-Allow-Origin, origin)

--- 2.cors/service-end.js
+++ 2.cors/service-end.js
@@ -2,7 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const whiteList = ["http://localhost:3001"];
 app.get("/getData", (req, res) => {
+  const origin = req.headers.origin;
+  if (whiteList.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.end("getdata 接口返回的数据11");
 });
 
