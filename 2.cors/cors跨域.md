a0f209ee23d3f014c005a9c5667f4799044072df
Cherish0207 <cheshire_1207@foxmail.com>
2021-02-17 16:24:51 +0800

    cors.5.允许携带 cookie,设置允许返回的头

--- 2.cors/cors跨域.md
+++ 2.cors/cors跨域.md
@@ -30,5 +30,24 @@ res.setHeader("Access-Control-Allow-Headers", "name1");
 
 #### 4. 复杂请求跨域
 
+put 在 restful 风格接口里代表修改
 put 请求是可以发送请求体的，想跨域，要先发送一个预警，看能否能跨域
 如果是 options 请求时，不作处理
+
+#### 5.允许携带 cookie,设置允许返回的头
+
+```js
+// 客户端
+document.cookie = "name=crx";
+xhr.withCredentials = true;
+
+// 服务端
+// 允许携带cookie
+res.setHeader("Access-Control-Allow-Credentials", true);
+// 允许返回的头
+res.setHeader("Access-Control-Expose-Headers", "name");
+res.setHeader("name", "svs");
+```
+
+如果 Access-Control-Allow-Origin 设置为 _，表示任何人都能访问，
+_ 当设置为 \* 时，不能和 Access-Control-Allow-Credentials 共用
--- 2.cors/service-end.js
+++ 2.cors/service-end.js
@@ -6,6 +6,10 @@ const whiteList = ["http://localhost:3001"];
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (whiteList.includes(origin)) {
+    /**
+     * 如果 Access-Control-Allow-Origin 设置为 *，表示任何人都能访问，
+     * 当设置为 * 时，不能和 Access-Control-Allow-Credentials 共用
+     */
     res.setHeader("Access-Control-Allow-Origin", origin);
     // 允许携带哪个头访问我
     res.setHeader("Access-Control-Allow-Headers", "name1,name2");
@@ -13,6 +17,10 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "PUT");
     // 预检的存活时间
     res.setHeader("Access-Control-Max-Age", 6);
+    // 允许携带cookie
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    // 允许返回的头
+    res.setHeader("Access-Control-Expose-Headers", "name");
     if (req.method === "OPTIONS") {
       res.end();
     }
@@ -20,6 +28,8 @@ app.use((req, res, next) => {
   next();
 });
 app.put("/getData", function (req, res) {
+  console.log(req.headers);
+  res.setHeader("name", "svs");
   res.end("getdata put 接口返回的数据222");
 });
 app.get("/getData", (req, res) => {
