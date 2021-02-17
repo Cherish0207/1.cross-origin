35c6fbe79353cecc86828c894c6d11e2f693df51
Cherish0207 <cheshire_1207@foxmail.com>
2021-02-17 15:33:55 +0800

    cors.2.服务端-中间件

--- 2.cors/service-end.js
+++ 2.cors/service-end.js
@@ -3,12 +3,15 @@ const express = require("express");
 const app = express();
 
 const whiteList = ["http://localhost:3001"];
-app.get("/getData", (req, res) => {
+app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (whiteList.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  res.end("getdata 接口返回的数据11");
+  next();
+});
+app.get("/getData", (req, res) => {
+  res.end("getdata 接口返回的数据111");
 });
 
 app.listen(3002);
